ab2f20b66bac700a8fc108cee8e13e59f1c264c1
Martin Sosic <sosic.martin@gmail.com>
2014-03-03 02:41:35 +0100

    Added cudaminer shutdown on script exit

--- mine.sh
+++ mine.sh
@@ -11,26 +11,36 @@ STRATUM=FILL_THIS
 MAX_TEMP=80
 
 
+# Returns GPU temperature
 function gpu_temp() {
 	nvidia-smi -q -d TEMPERATURE | grep Gpu | grep -P -o "[0-9]+"
 }
 
-echo "Starting mining!"
+# Kills process. First sends SIGTERM, then SIGKILL if needed.
+# Takes one argument: PID
+function kill_gracefully() {
+	kill $1
+	sleep 1
+	if [ -d /proc/$1 ]; then
+		kill -9 $1
+	fi
+}
 
+
+
+# start cudaminer
 ./cudaminer -H 1 -i 0 -l auto -C 1 -o $STRATUM -O $USERNAME.$WORKERNAME:$WORKERPASS &
 MINERPID=$!
 
+# Kill cudaminer if script ends
+trap "kill_gracefully $MINERPID" EXIT
+
+# Check GPU temperature and end script if GPU becomes too hot
 while true; do
 	TEMP=`gpu_temp`
-	echo $TEMP
 	if [ $TEMP -gt $MAX_TEMP ]; then
 		echo "GPU temperature got over $MAX_TEMP°C! Killing cuda miner"
-		kill $MINERPID
-		sleep 1
-		if [ -d /proc/$MINERPID ]; then
-			kill -9 $MINERPID
-		fi
-		break
+		exit
 	fi
 	sleep 1
 done
